29e7cbcdd1a383da06a04af75c261b6f7db45fa0
amandabeers <amanda.green413@gmail.com>
2019-06-26 13:02:54 -0400

    Add turn checking
    Added functions to mark squares based on whose turn it is and a function
    to toggle the current turn.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -1,16 +1,43 @@
 'use strict'
 
-const checkSquare = event => {
-  const square = event.target
-  console.log(square, $(square).text())
+const checkSquare = square => {
+  // const square = event.target
+  // console.log(square, $(square).text())
   if ($(square).text() === 'X' || $(square).text() === 'O') {
-    console.log('That move is invalid!')
+    return true
+  } else {
+    return false
+  }
+}
+
+let currentTurn = 'playerX'
+
+const toggleTurn = () => {
+  if (currentTurn === 'playerX') {
+    currentTurn = 'playerO'
+  } else if (currentTurn === 'playerO') {
+    currentTurn = 'playerX'
+  }
+}
+
+const markSquare = (square) => {
+  if (currentTurn === 'playerX') {
+    $(square).text('X')
+  } else {
+    $(square).text('O')
   }
 }
 
 const onClickBoard = event => {
   event.preventDefault()
-  checkSquare(event)
+  const square = event.target
+  const isItMarked = checkSquare(square)
+  if (isItMarked) {
+    console.log('That move is invalid!')
+  } else {
+    markSquare(square)
+    toggleTurn()
+  }
   // const square = event.target
   // console.log(square, $(square).text())
   // if ($(square).text() === 'X' || $(square).text() === 'O') {
